077bf555e965cbeff06616e5fa70f88893060edb
sakamotoyoh <zm25357@josai.ac.jp>
2025-10-14 09:45:46 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -3,6 +3,6 @@ alert(1+1);
 alert(Math.sqrt(1+1));
 alert(2+3);
 alert('2'+'3');
-alert(Number('2')+nUMBER('3');
-alert('2'+3);
+alert(Number('2')+Number('3');
+alert(('2')+3);
 alert(Number('2')+3);
