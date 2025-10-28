25d290cbfda68c650703b350489572235fdc413d
sakamotoyoh <zm25357@josai.ac.jp>
2025-10-28 09:49:46 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -1,5 +1,11 @@
+var who65= 'world';
 function sayhello61(){
-   alert('Hello, Function!');
+   alert('Hello, ' + who65 +'!');
+}
+function taro65(){
+   who65='太郎';
+function hanako65(){
+   who65='花子';
 }
 function saygoodbye61(){
    alert('Goodbye.');
