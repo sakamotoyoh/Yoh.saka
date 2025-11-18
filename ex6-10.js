a5ab18d7372749ea6c3fb676c263afa1600ec849
sakamotoyoh <zm25357@josai.ac.jp>
2025-11-18 11:55:50 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,3 @@
 function gazo610()
 {document.getElementByld('who610').innerHTML=
-  '<image src="./image/download.jpg" alt="オン">'
- ;}
+  '<image src="./image/download.jpg" alt="すき焼き" width="200">' ;}
