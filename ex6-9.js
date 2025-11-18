6966428dcac4a04437db9df8474d1a2ee41dfcab
sakamotoyoh <zm25357@josai.ac.jp>
2025-11-18 09:57:13 +0900

    Update ex6-9.js

--- ex6-9.js
+++ ex6-9.js
@@ -1,6 +1,5 @@
 function sayhello69(){
-  alert('Hello,' + document.getElementByld('who69').innerHTML + 
-'!');
+  alert('Hello,' + document.getElementByld('who69').innerHTML + '!');
 }
 function taro69(){
   document.getElementByld('who69').innerHTML = '太郎';
