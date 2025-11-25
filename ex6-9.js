f4b75f013d1f32b0f1e503aecdc86813e537a355
sakamotoyoh <zm25357@josai.ac.jp>
2025-11-25 10:21:42 +0900

    Update ex6-9.js

--- ex6-9.js
+++ ex6-9.js
@@ -1,10 +1,10 @@
 function sayhello69(){
-   alert('Hello,' + document.getElementByld('who69').innerHTML + 
+   alert('Hello,' + document.getElementById('who69').innerHTML + 
 '!');
 }
 function taro69(){
-  document.getElementByld('who69').innerHTML = '太郎';
+  document.getElementById('who69').innerHTML = '太郎';
 }
 function hanako69(){
-  document.getElementByld('who69').innerHTML = '花子';
+  document.getElementById('who69').innerHTML = '花子';
 }
