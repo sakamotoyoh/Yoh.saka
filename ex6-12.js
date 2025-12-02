c1f78e3813f22f6bcfd2817ac5b33a0e63c4a854
sakamotoyoh <zm25357@josai.ac.jp>
2025-12-02 09:58:15 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,4 +1,3 @@
 function imgchange1(){
-  '<image src="./image/download.jpg" alt="すき焼き" width="200">
-  '<img src="./image/sushi_01.png" alt="寿司" width="200">'
+  '<image src="./image/download.jpg" alt="すき焼き" width="200">'
   ;}
