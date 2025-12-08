1d6ea66e7dc32882c4fc97fcd56c2a3f2a3f5e89
sakamotoyoh <zm25357@josai.ac.jp>
2025-12-08 19:58:40 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,6 +1,7 @@
 function gazo612(){
-  document.getElementById('who&!"img').src = './image/download.jpg';
+  document.getElementById('who612img').src = './image/download.jpg';
 }
+
 function gazoReset612(){
-  document.getElementById('who&!"img').src = './image/sushi_01.png';
+  document.getElementById('who612img').src = './image/sushi_01.png';
 }
