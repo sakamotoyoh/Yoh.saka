d7c35d527bc5ffe2a8d680b80af4ad11287cddce
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-24 23:50:13 +0900

    Update ex6-17.js

--- ex6-17.js
+++ ex6-17.js
@@ -1,13 +1,14 @@
-function hyoji17(){
+function hyouji17(){
 //表示する文字
-var  str17="①一文字ずつ表示します。";
-  //テキストボックスの文字数
-var cnt17 = document.timer17.moji17.value.length;
-  //文字が表示されているか確認
-if(cnt < 11){
-  //現在予知１文字多く切り出して表示
-document.timer17.moji17.value = str17.substr(0,cnt17+1);} else{
-//すべてを表示されたら、空文字に戻す
+var str17 = "１文字ずつ表示します。";
+//テキストボックスの文字数
+var cnt17 = document.timer17.moji!'.value.length;
+//文字が全部表示されているか確認する
+if( cut17<11){
+//現在より１文字多く切り出して表示
+document.timer17.moji17.value = str17.substr(0,cnt17+1);}
+else{
+//全て表示させたら、空文字に戻す
 document.timer17.moji17.value = "";}
 //setTimeout()を含む関数を呼び出す
-setTimeout("hyoji17()",1000);}
+setTimeout("hyouji17()",1000);}
