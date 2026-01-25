d86665fc36bfcf16d452b0a8d7078efdd6e30b37
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-26 00:11:08 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,1 +1,15 @@
-
+function nazo(){
+  if(document.getElementById('answer').value == '夢'){
+    alert('あたり!');
+  }
+  else{
+    alert('ハズレ')
+  }
+}
+ if(document.getElementById('answer').value =='ゆめ'){
+   alert('あたり');
+ }
+ else{
+   alert('ハズレ')
+  }
+}
