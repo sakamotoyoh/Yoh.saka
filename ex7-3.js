7e981f0532fda4b643c74bc4aa7e1659fe939ac2
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-26 00:43:04 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -11,15 +11,3 @@ function calculateBMI() {
 
   // 4. 結果をHTMLに表示（小数点第2位まで表示する例）
   document.getElementById('result_bmi').textContent = bmi.toFixed(2);
-
-  // 5. 判定結果を表示する（応用編）
-  let status = '';
-  if (bmi < 18.5) {
-    status = 'やせ型';
-  } else if (bmi >= 18.5 && bmi < 25) {
-    status = '普通体重';
-  } else {
-    status = '肥満'; // BMI 25以上は肥満と判定される
-  }
-  document.getElementById('result_status').textContent = status;
-}
