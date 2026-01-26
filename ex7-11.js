98c62ac0c48f01f08b4043f266c6836b7e82c4a9
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-26 15:37:57 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,20 @@
+function checkYear() {
+  let year = document.getElementById("year").value;
+  year = Number(year);
 
+  let message = "";
+
+  // うるう年の判定
+  if (year % 400 === 0) {
+    message = "うるう年です";
+  } else if (year % 100 === 0) {
+    message = "平年です";
+  } else if (year % 4 === 0) {
+    message = "うるう年です";
+  } else {
+    message = "平年です";
+  }
+
+  document.getElementById("result").textContent =
+    year + "年は" + message;
+}
