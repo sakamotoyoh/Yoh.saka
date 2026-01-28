514813184189200c46b1e33b1a5ffb1d3d5e78fb
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-28 21:52:37 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,9 +1,9 @@
 // 写真を2枚目に切り替える関数
 function changePhoto() {
-  document.getElementById("myPhoto").src = "images/photo2.jpg";
+  document.getElementById("myPhoto").src = "image/WIN_20260128_21_29_27_Pro.jpg";
 }
 
 // 元の写真に戻す関数
 function resetPhoto() {
-  document.getElementById("myPhoto").src = "images/photo1.jpg";
+  document.getElementById("myPhoto").src = "image/WIN_20260128_21_29_25_Pro.jpg";
 }
