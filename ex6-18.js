263c4bb3a4bab3505ba98219a5d6f7c1d1ea8826
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-28 22:08:32 +0900

    Update ex6-18.js

--- ex6-18.js
+++ ex6-18.js
@@ -1,13 +1,11 @@
-// 画像を切り替える関数
 function changePhoto() {
-  // 画像を2枚目に変更
-  document.getElementById("myPhoto").src = "images/WIN_20260128_21_45_54_Pro.jpg";
+  document.getElementById("myPhoto").src =
+    "images/MINI_20260128_21_45_54_Pro.jpg";
 
-  // 3秒後に元の画像へ戻す
   setTimeout(returnPhoto, 3000);
 }
 
-// 元の画像に戻す関数
 function returnPhoto() {
-  document.getElementById("myPhoto").src = "images/WIN_20260128_21_45_56_Pro.jpg";
+  document.getElementById("myPhoto").src =
+    "images/MINI_20260128_21_45_56_Pro.jpg";
 }
