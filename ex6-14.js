ea19c4c071bc258ca4dd39957615c7ba16439948
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-28 22:31:12 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -2,9 +2,9 @@
 const myImage = document.getElementById('myPhoto');
 
 // 最初の写真のファイル名を定義します
-const photo1Src = 'my_face_closed.jpg'; // 自分の最初の写真のファイル名に変更してください
+const photo1Src = 'image/WIN_20260128_21_45_56_Pro.jpg'; // 自分の最初の写真のファイル名に変更してください
 // 2枚目の写真のファイル名を定義します
-const photo2Src = 'my_face_open.jpg'; // 自分の2枚目の写真のファイル名に変更してください
+const photo2Src = 'image/WIN_20260128_21_45_54_Pro.jpg'; // 自分の2枚目の写真のファイル名に変更してください
 
 // 写真を2枚目のものに切り替える関数
 function changeToPhoto2() {
