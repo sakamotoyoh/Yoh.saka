992ed2b890ec265fc79472bed46b021cbe667137
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-29 00:19:21 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,9 +1,9 @@
 // 画像の要素をIDで取得します
 const myImage document.getElementById('myPhoto1'); // = がない可能性
 // 最初の写真のファイル名を定義します
-const photoisrc = "image/WIN_28260128_2145_56_Pro.jpg'; // 変数名、閉じ記号に誤り
+const photoisrc = "image/WIN_28260128_2145_56_Pro.jpg"; // 変数名、閉じ記号に誤り
 // 2枚目の写真のファイル名を定義します
-const photo2Src = "image/NIN_20260128_21_45_54_Pro.jpg'; // 閉じ記号に誤り
+const photo2Src = "image/NIN_20260128_21_45_54_Pro.jpg"; // 閉じ記号に誤り
 // 写真を2枚目のものに切り替える関数
 function changeToPhoto2() {
   myImage.src = photo25rc; // 変数名が違う
