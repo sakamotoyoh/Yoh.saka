3aba4f34c12fc2886906957fc58e6c63b69c83e0
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-28 23:31:07 +0900

    Update ex7-8.js

--- ex7-8.js
+++ ex7-8.js
@@ -1,8 +1,11 @@
-function nazo78(){
-  if(document.getElementById('answer78').value === '夢'){
+function nazo78() {
+  const answerValue = document.getElementById('answer78').value.trim();
+
+  // 正解が「夢」の場合のみ「あたり！」とする
+  if (answerValue === '夢') {
+    alert('あたり!');
+  } else {
+    // それ以外（「ゆめ」を含む）の場合は「はずれ！」
     alert('はずれ!');
-  }
-  else{
-    alert('あたり');
   }
 }
