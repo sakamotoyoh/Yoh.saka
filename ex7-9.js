8f366646d936360799d1d1348f75b1395231377b
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-28 23:18:41 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,10 +1,12 @@
 function nazo79() {
-  const answerValue = document.getElementById('answer79').value; // 値を変数に入れて再利用
+  const answerValue = document.getElementById('answer79').value; // 値を取得
 
-  if (answerValue === '夢') { // 比較演算子 === を使うのがより良いプラクティスです
+  if (answerValue === '夢') { // 厳密な等価性比較 (===) を使用
     alert('あたり!');
-  } else if (answerValue === 'ゆめ') {
+  } else if (answerValue === 'ゆめ') { // こちらも === に変更
     alert('あたり!');
   } else {
     alert('ハズレ'); // 上記いずれにも当てはまらない場合のハズレ
   }
+}
+
