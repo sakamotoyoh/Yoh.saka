e576bea5743dd8519f79bcd018dd3ad6f62fec97
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-30 16:06:20 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,20 +1,20 @@
-// 画像の要素をIDで取得（HTML側で id="myPhoto1" を設定していること）
-const myImage = document.getElementById("myPhoto1");
+// 画像の要素をIDで取得(HTMLで id="myphotos" を設定していること)
+const myImage = document.getElementById("myphotos");
 
-// 写真ファイルのパス（HTML・実ファイル名と完全一致させる）
-const photo1Src = "image/MIN_20260128_21_45_56_Pro.jpg";
-const photo2Src = "image/MIN_20260128_21_45_54_Pro.jpg";
+// 写真ファイルのパス(HTML・実ファイル名と完全一致させる)
+const photossrc = "Image/20200128_21_45_54_Pro.jpg";
+const photo2src = "Image/MINI_20200128_21_45_54_Pro.jpg";
 
 // 写真を2枚目に切り替える関数
 function changeToPhoto2() {
-  if (myImage) {
-    myImage.src = photo2Src;
-  }
+    if (myImage) {
+        myImage.src = photo2src;
+    }
 }
 
 // 写真を1枚目に戻す関数
 function changeToPhoto1() {
-  if (myImage) {
-    myImage.src = photo1Src;
-  }
+    if (myImage) {
+        myImage.src = photossrc;
+    }
 }
