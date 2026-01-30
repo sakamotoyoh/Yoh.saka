19c2105447fe10d55e826ec3933caf53f88a6ec3
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-30 16:22:09 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,8 +1,19 @@
+// Yohsaka /ex6-14.js の正確なコード
+
 function changePhoto614() {
-  document.getElementById("myPhoto1").src =
-    "image/WIN_20260128_21_45_56_Pro.jpg";
+  // getElementById のスペルと大文字小文字を修正
+  const myImage = document.getElementById("myPhoto1");
+  if (myImage) {
+    // src属性の後にあった余分な文字を削除
+    myImage.src = "Image/MIN_20260128_21_45_56_Pro.jpg";
+  }
+}
 
 function returnPhoto614() {
-  document.getElementById("myPhoto1").src =
-    "image/WIN_20260128_21_45_54_Pro.jpg";
+  // getElementById のスペルと大文字小文字を修正
+  const myImage = document.getElementById("myPhoto1");
+  if (myImage) {
+    // src属性の後にあった余分な文字を削除
+    myImage.src = "image/MIN_20260128_21_45_54_Pro.jpg";
+  }
 }
