06dabf95cb05d9a3ca7e574e8b38eea7376ea8f1
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-30 14:34:25 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,15 +1,14 @@
 function calcBMI() {
-  // 身長と体重を取得
-  let height = document.getElementById("height1").value;
-  let weight = document.getElementById("weight1").value;
+    // 1. 入力された身長と体重の値を取得する
+    const h = document.getElementById('height1').value;
+    const w = document.getElementById('weight1').value;
 
-  // mに変換するため数値型に変換
-  // Number() 関数を使用して文字列を数値に変換しています
-  height = Number(height) / 100;
+    // 2. 身長をcmからmに変換する
+    const hm = h / 100;
 
-  // 計算
-  let bmi = weight / (height * height);
+    // 3. BMIを計算する（体重 ÷ 身長の2乗）
+    const bmi = w / (hm * hm);
 
-  // 結果を表示(小数第2位まで)
-  document.getElementById("result1").textContent = "あなたのBMIは" + bmi.toFixed(2) + "です";
+    // 4. 結果を表示する
+    document.getElementById('result').innerText = 'BMIは' + bmi + 'です';
 }
